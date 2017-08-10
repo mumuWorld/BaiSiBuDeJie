8e89aa11dc186d324a0a5f8c35f0b80ad3b85fdc
yangjie <617958070@qq.com>
2017-08-10 23:13:55 +0800

    Initial Commit

---
+++ 百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by mumu on 2017/8/10.
+//  Copyright © 2017年 mumu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
